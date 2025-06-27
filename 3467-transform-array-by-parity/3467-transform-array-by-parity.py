e1b50c21f0971a726af970c2ec5a44228ad99811
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2025-06-26 23:57:26 -0700

    Time: 1 ms (59.8%), Space: 12.6 MB (13.12%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def transformArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return sorted([0 if num%2 == 0 else 1 for num in nums])
+        
