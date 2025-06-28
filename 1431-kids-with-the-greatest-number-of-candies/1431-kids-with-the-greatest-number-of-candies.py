4d3c6b849ccbffee80eec7277a0a1e4ca8c6b10b
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2025-06-27 23:33:30 -0700

    Time: 7 ms (8.81%), Space: 12.3 MB (82.56%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        return [ x + extraCandies >= max(candies) for x in candies]
+        
