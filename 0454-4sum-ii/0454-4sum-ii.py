3c9956a7941ac3cea55ef72d81564034b6f1f4bf
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2025-07-25 21:04:44 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type nums3: List[int]
+        :type nums4: List[int]
+        :rtype: int
+        """
+        result = 0
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                for k in range(len(nums3)):
+                    for l in range(len(nums4)):
+                        if nums1[i]+nums2[j]+nums3[k]+nums4[l] == 0:
+                            result += 1
+
+        return result
+        
