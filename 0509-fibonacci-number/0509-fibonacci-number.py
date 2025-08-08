20396bfd1e5afdb66b78d76f2f968ab1c432ace7
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2025-08-08 00:07:55 -0700

    Time: 19 ms (56.26%), Space: 12.5 MB (9.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,15 +6,13 @@ class Solution(object):
         """
         if n <= 1:
             return n
-            
-        dp = [0]* (n+1)
-        dp[0] = 0
-        dp[1] = 1
 
+        a, b = 0,1
 
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        for _ in range(2, n+1):
 
-        return dp[n]
+            a,b = b, a+b
+
+        return b
 
 
