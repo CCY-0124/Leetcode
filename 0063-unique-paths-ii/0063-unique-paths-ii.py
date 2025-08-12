9ec9c85b62239ff0793ce64e418dcf7df4b4a0be
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2025-08-11 17:49:58 -0700

    Time: 19 ms (6.5%), Space: 12.5 MB (35.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,24 +4,18 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        if obstacleGrid[0][0] == 1:
-            return 0
-
         n = len(obstacleGrid[0])
         m = len(obstacleGrid)
+
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
+            return 0
         
         dp =[[0] * n for _ in range(m)]
-
-        for i in range(m):
-            if obstacleGrid[i][0] == 1:
-               dp[i][0] = 0
-            else: 
-                dp[i][0] = 1
-        for j in range(n):
-            if obstacleGrid[0][j] == 1:
-               dp[0][j] = 0
-            else: 
-                dp[0][j] = 1
+        dp[0][0] = 1 
+        for i in range(1, m):
+            dp[i][0] = 0 if obstacleGrid[i][0] == 1 else dp[i-1][0]
+        for j in range(1, n):
+            dp[0][j] = 0 if obstacleGrid[0][j] == 1 else dp[0][j-1]
 
         for i in range(1, m):
             for j in range(1, n):
@@ -29,6 +23,8 @@ class Solution(object):
                     dp[i][j] = 0
                 else:
                     dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        print(dp)
 
         return dp[m-1][n-1]
         
