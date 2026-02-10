ba487551b8ea3178fe3e3c02367003432c4b6919
Chui Ying Chung <118567509+CCY-0124@users.noreply.github.com>
2026-02-09 22:31:07 -0800

    Time: 0 ms (100%), Space: 12.5 MB (18.92%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def alternatingSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(nums[::2])-sum(nums[1::2])
+        
